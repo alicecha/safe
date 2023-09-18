ee5986e29bb57abb741f3cbd1cf91acd472fb60c
alicecha <110170331+alicecha@users.noreply.github.com>
2023-09-18 16:43:39 +0300

    Added script that returns number of walletconnect transactions for safe

--- query.py
+++ query.py
@@ -1,8 +1,24 @@
-# Initialise variable safe_address, value = 0xBbA4C8eB57DF16c4CfAbe4e9A3Ab697A3e0C65D8
-# Initialise list walletconnect_txs, value = []
+import requests
 
-# Make API call to https://safe-transaction-mainnet.safe.global/api/v1/safes/{safeAddress}/multisig-transactions/ and save results
+safe_address = '0xBbA4C8eB57DF16c4CfAbe4e9A3Ab697A3e0C65D8'
+walletconnect_txs = []
 
-# Iterate over results, saving tx hash where contain "WalletConnect" in origin field into walletConnectTxs
+def main():
+    walletconnect_txs = fetch_wc_txs(call_api(safe_address))
+    print(f"The safe {safe_address} has successfully executed {len(walletconnect_txs)} WalletConnect transactions.")
 
-# Count number tx hashes saved
+# Returns results from API call
+def call_api(address):
+    return requests.get(f"https://safe-transaction-mainnet.safe.global/api/v1/safes/{address}/multisig-transactions/").json()
+
+# Returns hashes for txs **successfully** executed through WalletConnect 
+def fetch_wc_txs(response):
+    txs = []
+    for tx in response["results"]:
+        if tx["origin"].find("WalletConnect") >= 0:
+            if tx["isSuccessful"] is not None:
+                txs.append(tx["transactionHash"])
+    return txs
+
+if __name__ == "__main__":
+    main()
